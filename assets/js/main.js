85de544bf3fb557fb87140356e1944530ea36000
Brilly Majuan <sbmajuanl@gmail.com>
2017-02-23 16:58:38 -0500

    Ejercicio2_L13

---
+++ assets/js/main.js
@@ -0,0 +1,14 @@
+
+var array=[ { nombre : "Arabela", apellido : "Rojas", rol : "T.A", cumpleanios : "23 de enero" },
+{ nombre : "Michelle", apellido : "Seguil", rol : "T.A", cumpleanios : "25 de noviembre" },
+{ nombre : "Meche", apellido : "Zubieta", rol : "T.A", cumpleanios : "02 de marzo" },
+{ nombre : "Papu", apellido : "Rivariola", rol : "Psicóloga", cumpleanios : "10 de octubre" },
+{ nombre : "Gian", apellido : "Corzo", rol : "Profesor", cumpleanios : "23 de enero" } ];
+
+var envio=document.getElementById('envio')
+array.forEach(function(e){
+envio.innerHTML+="<div><ul><li>Nombre: "+e.nombre+
+                      "</li><li>Apellidos: "+e.apellido+
+                      "</li><li>Rol: "+e.rol+
+                      "</li><li>Cumpleaños: "+ e.cumpleanios+"</li></ul></div>";
+});
